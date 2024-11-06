bdbf6f4bbe2be24cbf7a2772bff2d2c93679dcd6
Fran Braulio Muñoz <franbrauliom@gmail.com>
2024-11-06 09:40:06 +0100

    Herencia

--- Unidad4/EntregaPracticaPOO/Entrenamiento.js
+++ Unidad4/EntregaPracticaPOO/Entrenamiento.js
@@ -9,6 +9,8 @@ class Vehiculo{
     mostrarInfo(){
         return `${this.brand} ${this.model}, año${this.manufacturingYear}, ${this.price}€`;
     }
+
+
 }
 
 class Coche extends Vehiculo{
@@ -23,15 +25,14 @@ class Coche extends Vehiculo{
 }
 
 class Motos extends Vehiculo{
-    constructor(brand, model, manufacturingYear, price, cruiser, recto, tracker ){
+    constructor(brand, model, manufacturingYear, price, tipoDeManillar ){
         super(brand, model, manufacturingYear, price);
-        this.cruiser = cruiser;
-        this.recto = recto;
-        this.tracker = tracker;
+        this.tipoDeManillar = tipoDeManillar;
+
     }
 
     mostrarInfo(){
-        return `${super.mostrarInfo()} cruiser ${this.cruiser}, recto ${this.recto}, tracker ${this.tracker}`;
+        return `${super.mostrarInfo()} tipo de manillar ${this.tipoDeManillar}`;
     }
 }
 
@@ -55,7 +56,7 @@ class Concesionario{
 const vehiculo1 = new Vehiculo("Toyota", "Corolla", 2019, 15000);
 const coche1 = new Coche("Honda", "Civic", 2020, 18000, 4);
 const coche2 = new Coche("Ford", "Mustang", 2021, 30000, 2);
-const moto1 = new Motos("Yamaha", "MT-07", 2021, 7500, true, false, false);
+const moto1 = new Motos("Yamaha", "MT-07", 2021, 7500, "tracker");
 console.log("INFORMACION DE UNA MOTO ESPECIFICA");
 console.log(moto1.mostrarInfo());
 
@@ -67,7 +68,7 @@ console.log("LISTADO DEL CONCESIONARIO");
 concesionario.listadoDeAlmacen();
 
 // Agregar un vehículo adicional
-const moto2 = new Motos("Harley-Davidson", "Iron 883", 2019, 9000, true, true, false);
+const moto2 = new Motos("Harley-Davidson", "Iron 883", 2019, 9000, "cruiser");
 concesionario.addVehiculo(moto2);
 
 // Listar los vehículos en el concesionario
