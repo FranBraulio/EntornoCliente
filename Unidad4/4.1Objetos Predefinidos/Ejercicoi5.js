14ad3ba3af0ea5c38e207df042316af594ad4a2a
Fran Braulio Muñoz <franbrauliom@gmail.com>
2024-11-13 10:59:52 +0100

    A mitad

---
+++ Unidad4/4.1Objetos Predefinidos/Ejercicoi5.js
@@ -0,0 +1,9 @@
+let fechaActual = new Date();
+console.log(fechaActual.getHours());
+console.log(fechaActual.getHours()-6);
+console.log(fechaActual.getHours()+10);
+console.log(fechaActual.getHours()+10);
+console.log(fechaActual.getHours()-1);
+
+
+
